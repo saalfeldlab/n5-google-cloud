38cbf5a91cbde04f8045dc277ce8af0f24671833
Igor Pisarev <pisarevi@janelia.hhmi.org>
2020-04-07 12:51:48 -0400

    suppress sdk warning about authenticating with end-user credentials
    this makes sense for running on Google Cloud where service accounts are used, but there is nothing wrong with using end-user credentials on a local machine (and in fact this is the preferred way)

--- src/main/java/org/janelia/saalfeldlab/googlecloud/GoogleCloudClient.java
+++ src/main/java/org/janelia/saalfeldlab/googlecloud/GoogleCloudClient.java
@@ -4,6 +4,8 @@ import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Map;
 
 public abstract class GoogleCloudClient<T> {
 
@@ -18,6 +20,30 @@ public abstract class GoogleCloudClient<T> {
 
 	public static Credentials getSystemCredentials() throws IOException {
 
+		suppressCredentialsWarning();
 		return GoogleCredentials.getApplicationDefault();
 	}
+
+	/**
+	 * Google Cloud SDK prints a warning about authenticating using end user credentials instead of service accounts.
+	 *
+	 * While this makes sense for running the code on Google Cloud itself, there is nothing wrong with using
+	 * end user credentials on a local machine generated with 'gcloud auth'.
+	 *
+	 * To suppress this warning, an environment variable needs to be set. This method automates it by setting
+	 * the environment variable with reflection.
+	 */
+	@SuppressWarnings({ "unchecked" })
+	private static void suppressCredentialsWarning() {
+
+		try {
+			final Map<String, String> env = System.getenv();
+			final Field field = env.getClass().getDeclaredField("m");
+			field.setAccessible(true);
+			((Map<String, String>) field.get(env)).put(SUPPRESS_GCLOUD_CREDS_WARNING_ENV_VAR, Boolean.TRUE.toString());
+		} catch (final ReflectiveOperationException e) {
+			e.printStackTrace();
+		}
+	}
+	private static final String SUPPRESS_GCLOUD_CREDS_WARNING_ENV_VAR = "SUPPRESS_GCLOUD_CREDS_WARNING";
 }
