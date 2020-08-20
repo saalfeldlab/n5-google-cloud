5892dc9f8dd56880b8be078945360019a4fb89c1
bogovicj <bogovicj@janelia.hhmi.org>
2020-08-20 11:16:59 -0400

    parse project id from url query
    * and fix tests

--- src/main/java/org/janelia/saalfeldlab/googlecloud/GoogleCloudStorageURI.java
+++ src/main/java/org/janelia/saalfeldlab/googlecloud/GoogleCloudStorageURI.java
@@ -1,14 +1,20 @@
 package org.janelia.saalfeldlab.googlecloud;
 
 import java.net.URI;
+import java.util.Map;
+
+import com.google.common.base.Splitter;
 
 public class GoogleCloudStorageURI
 {
 	private static final String googleCloudHost = "googleapis.com";
 	private static final String storagePathPrefix = "/storage/v1/b/";
+	private static final String projectKey = "project";
 
 	private final String bucketName;
 	private final String objectKey;
+	private final String query;
+	private Map< String, String > queryMap;
 
 	public GoogleCloudStorageURI( final String str )
 	{
@@ -22,9 +28,11 @@ public class GoogleCloudStorageURI
 		{
 			bucketName = uri.getAuthority();
 			objectKey = uri.getPath();
+			query = uri.getQuery();
 		}
 		else if ( uri.getScheme().equalsIgnoreCase( "http" ) || uri.getScheme().equalsIgnoreCase( "https" ) )
 		{
+
 			if ( !uri.getHost().equalsIgnoreCase( googleCloudHost ) && !uri.getHost().equalsIgnoreCase( "www." + googleCloudHost ) )
 				throw new IllegalArgumentException( "Not a google cloud storage link" );
 
@@ -32,15 +40,17 @@ public class GoogleCloudStorageURI
 				throw new IllegalArgumentException( "Not a google cloud storage link" );
 
 			path = uri.getPath().substring( storagePathPrefix.length() );
-
 			final int delimeterIndex = path.indexOf( "/" );
+
 			bucketName = path.substring( 0, delimeterIndex != -1 ? delimeterIndex : path.length() );
-			objectKey = delimeterIndex != -1 && delimeterIndex < path.length() - 1 ? path.substring( delimeterIndex + 1 ) : null;
+			objectKey = delimeterIndex != -1 && delimeterIndex < path.length() - 1 ? path.substring( delimeterIndex ) : "";
+			query = uri.getQuery();
 		}
 		else
 		{
 			throw new IllegalArgumentException( "Invalid scheme" );
 		}
+		queryMap = parseQuery();
 
 	}
 
@@ -53,4 +63,26 @@ public class GoogleCloudStorageURI
 	{
 		return objectKey;
 	}
+
+	public String getQuery()
+	{
+		return query;
+	}
+	
+	public String getProject()
+	{
+		if( queryMap != null && queryMap.containsKey( projectKey ))
+			return queryMap.get( projectKey );
+
+		return null;
+	}
+
+	private Map<String,String> parseQuery()
+	{
+		if( query != null )
+			return Splitter.on( '&' ).trimResults().withKeyValueSeparator( '=' ).split( query );
+		else
+			return null;
+	}
+
 }
--- src/test/java/org/janelia/saalfeldlab/googlecloud/GoogleCloudStorageURITest.java
+++ src/test/java/org/janelia/saalfeldlab/googlecloud/GoogleCloudStorageURITest.java
@@ -15,11 +15,31 @@ public class GoogleCloudStorageURITest
 				"https://googleapis.com/storage/v1/b/test-bucket/test-dir/test-object.test",
 				"https://www.googleapis.com/storage/v1/b/test-bucket/test-dir/test-object.test"
 			};
+
 		for ( final String link : links )
 		{
 			final GoogleCloudStorageURI uri = new GoogleCloudStorageURI( link );
 			Assert.assertEquals( "test-bucket", uri.getBucket() );
-			Assert.assertEquals( "test-dir/test-object.test", uri.getKey() );
+			Assert.assertEquals( "/test-dir/test-object.test", uri.getKey() );
+			Assert.assertNull( "null query", uri.getQuery() );
+		}
+	}
+
+	public void testQuery()
+	{
+		final String[] links = new String[] {
+				"gs://test-bucket/test-dir/test-object.test?project=value",
+				"http://googleapis.com/storage/v1/b/test-bucket/test-dir/test-object.test?project=value",
+				"http://www.googleapis.com/storage/v1/b/test-bucket/test-dir/test-object.test?project=value",
+				"https://googleapis.com/storage/v1/b/test-bucket/test-dir/test-object.test?project=value",
+				"https://www.googleapis.com/storage/v1/b/test-bucket/test-dir/test-object.test?project=value"
+			};
+
+		for ( final String link : links )
+		{
+			final GoogleCloudStorageURI uri = new GoogleCloudStorageURI( link );
+			Assert.assertEquals( "project=value", uri.getQuery() );
+			Assert.assertEquals( "value", uri.getProject() );
 		}
 	}
 
@@ -38,11 +58,12 @@ public class GoogleCloudStorageURITest
 				"https://www.googleapis.com/storage/v1/b/test-bucket",
 				"https://www.googleapis.com/storage/v1/b/test-bucket/",
 			};
+
 		for ( final String link : links )
 		{
 			final GoogleCloudStorageURI uri = new GoogleCloudStorageURI( link );
 			Assert.assertEquals( "test-bucket", uri.getBucket() );
-			Assert.assertNull( uri.getKey() );
+			Assert.assertTrue( "path empty or root", uri.getKey().isEmpty() || uri.getKey().equals( "/" ));
 		}
 	}
 }
