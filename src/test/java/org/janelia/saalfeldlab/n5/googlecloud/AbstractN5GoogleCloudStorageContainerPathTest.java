75eb9cfda988b8d5820bd600ab90da454d26b93a
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2020-09-17 19:18:13 -0400

    clean up LICENSE blurb mess

--- src/test/java/org/janelia/saalfeldlab/n5/googlecloud/AbstractN5GoogleCloudStorageContainerPathTest.java
+++ src/test/java/org/janelia/saalfeldlab/n5/googlecloud/AbstractN5GoogleCloudStorageContainerPathTest.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -26,30 +26,15 @@
  * POSSIBILITY OF SUCH DAMAGE.
  * #L%
  */
-/**
- * License: GPL
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
 package org.janelia.saalfeldlab.n5.googlecloud;
 
-import com.google.cloud.storage.Storage;
+import java.io.IOException;
+
 import org.janelia.saalfeldlab.n5.N5Writer;
 import org.junit.AfterClass;
 import org.junit.Assert;
 
-import java.io.IOException;
+import com.google.cloud.storage.Storage;
 
 public abstract class AbstractN5GoogleCloudStorageContainerPathTest extends AbstractN5GoogleCloudStorageTest {
 
