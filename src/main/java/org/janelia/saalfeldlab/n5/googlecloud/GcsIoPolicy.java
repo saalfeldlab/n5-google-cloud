8b3a381984f5ff205121c0810a32be3834c66a56
Caleb Hulbert <cmhulbert@gmail.com>
2026-02-19 14:48:16 -0500

    feat: implement IoPolicy
    Signed-off-by: Caleb Hulbert <cmhulbert@gmail.com>

--- src/main/java/org/janelia/saalfeldlab/n5/googlecloud/GcsIoPolicy.java
+++ src/main/java/org/janelia/saalfeldlab/n5/googlecloud/GcsIoPolicy.java
@@ -1,16 +1,32 @@
 package org.janelia.saalfeldlab.n5.googlecloud;
 
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Storage;
+import com.google.api.gax.paging.Page;
+import com.google.cloud.ReadChannel;
+import com.google.cloud.storage.*;
 import org.janelia.saalfeldlab.n5.IoPolicy;
+import org.janelia.saalfeldlab.n5.N5Exception;
+import org.janelia.saalfeldlab.n5.readdata.LazyRead;
 import org.janelia.saalfeldlab.n5.readdata.ReadData;
 import org.janelia.saalfeldlab.n5.readdata.VolatileReadData;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 
 public interface GcsIoPolicy extends IoPolicy {
+
+    static boolean validBounds(long channelSize, long offset, long length) {
+
+        if (offset < 0)
+            return false;
+        else if (channelSize > 0 && offset >= channelSize) // offset == 0 and arrayLength == 0 is okay
+            return false;
+        else if (length >= 0 && offset + length > channelSize)
+            return false;
+
+        return true;
+    }
 
     class Unsafe implements GcsIoPolicy {
 
@@ -34,11 +50,31 @@ public interface GcsIoPolicy extends IoPolicy {
         @Override
         public VolatileReadData read(String key) {
 
-            return VolatileReadData.from(new GoogleCloudStorageKeyValueAccess.GCSLazyRead(storage, bucketName, key, false));
+            return VolatileReadData.from(new GCSLazyRead(storage, bucketName, key, false));
         }
 
         @Override
         public void delete(String key) {
+            if (!key.endsWith("/")) {
+                storage.delete(BlobId.of(bucketName, key));
+            }
+
+
+            /*
+             * TODO consider instead using Object Lifecycle Management when deleting many items see:
+             * https://cloud.google.com/storage/docs/deleting-objects#delete-objects-in-bulk
+             */
+            Page<Blob> page = storage.list(
+                    bucketName,
+                    Storage.BlobListOption.prefix(key),
+                    Storage.BlobListOption.fields(Storage.BlobField.ID));
+
+            while (page != null) {
+                final BlobId[] ids = page.streamValues().map(Blob::getBlobId).toArray(BlobId[]::new);
+                if (ids.length > 0) // storage throws an error if ids is empty
+                    storage.delete(ids);
+                page = page.getNextPage();
+            }
         }
     }
 
@@ -50,7 +86,102 @@ public interface GcsIoPolicy extends IoPolicy {
 
         @Override
         public VolatileReadData read(String key) {
-            return VolatileReadData.from(new GoogleCloudStorageKeyValueAccess.GCSLazyRead(storage, bucketName, key, true));
+            return VolatileReadData.from(new GCSLazyRead(storage, bucketName, key, true));
+        }
+    }
+
+    class GCSLazyRead implements LazyRead {
+
+        private final Storage storage;
+        private final String bucketName;
+        private final String normalKey;
+        private final boolean generationMatch;
+        private Long generation = null;
+
+
+        GCSLazyRead(
+                final Storage storage,
+                final String bucketName,
+                final String normalKey,
+                final boolean generationMatch) {
+            this.storage = storage;
+            this.bucketName = bucketName;
+            this.normalKey = normalKey;
+            this.generationMatch = generationMatch;
+        }
+
+        private Blob getBlob(String normalKey, Storage.BlobGetOption... options) {
+            final Blob blob;
+            try {
+                if (generationMatch && generation != null) {
+                    final Storage.BlobGetOption[] generationMatchOptions = new Storage.BlobGetOption[options.length + 1];
+                    System.arraycopy(options, 0, generationMatchOptions, 0, options.length);
+                    generationMatchOptions[options.length] = Storage.BlobGetOption.generationMatch(generation);
+                    BlobId blobId = BlobId.of(bucketName, normalKey, generation);
+                    blob = storage.get(blobId, generationMatchOptions);
+                } else {
+                    BlobId blobId = BlobId.of(bucketName, normalKey);
+                    blob = storage.get(blobId, options);
+                }
+            } catch (StorageException e) {
+                if (e.getCode() == 404)
+                    throw new N5Exception.N5NoSuchKeyException("No such key. bucket: " + bucketName + ". key: " + normalKey);
+                if (e.getCode() == 412)
+                    throw new N5Exception.N5ConcurrentModificationException("Generation mismatch. bucket: " + bucketName + ". key: " + normalKey);
+                throw e;
+            }
+
+            if (blob == null)
+                throw new N5Exception.N5NoSuchKeyException("No such key. bucket: " + bucketName + ". key: " + normalKey);
+
+            if (generationMatch && generation == null)
+                generation = blob.getGeneration();
+
+            return blob;
+        }
+
+        @Override
+        public long size() {
+
+            final Blob blob = getBlob(normalKey, Storage.BlobGetOption.fields(Storage.BlobField.SIZE));
+            return blob.getSize();
+        }
+
+        @Override
+        public ReadData materialize(final long offset, final long length) {
+
+            if (length > Integer.MAX_VALUE)
+                throw new N5Exception.N5IOException("Attempt to materialize too large data");
+
+            final Blob blob = getBlob(normalKey);
+            try (ReadChannel from = blob.reader()) {
+
+                final long channelSize = blob.getSize();
+                if (!validBounds(channelSize, offset, length))
+                    throw new IndexOutOfBoundsException();
+
+                from.seek(offset);
+                if (length > 0)
+                    from.limit(offset + length);
+
+                long readLength;
+                if (length < 0)
+                    readLength = channelSize;
+                else
+                    readLength = length;
+
+                final ByteBuffer buf = ByteBuffer.allocate((int) readLength);
+                from.read(buf);
+                return ReadData.from(buf);
+
+            } catch (IOException e) {
+                throw new N5Exception.N5IOException(e);
+            }
+        }
+
+        @Override
+        public void close() {
+            generation = null;
         }
     }
 }
